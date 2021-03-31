3ec92fd7a45bb79d84093dd967ca5162799c88e8
Lucas Borges <lscborges1@gmail.com>
2021-03-31 11:35:00 -0300

    tsconfig + initial typing

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -8,6 +8,7 @@ import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
 class Dashboard extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
